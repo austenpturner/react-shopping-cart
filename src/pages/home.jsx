e3785b06b38a4e99954acce6a8ba4366f344bca6
Austen Turner <austenpturner@msn.com>
2024-07-09 15:36:15 -0700

    fetch products and create products component files

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,37 @@
+import { useEffect, useState } from "react";
+import { Circles } from "react-loader-spinner";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchListOfProducts() {
+    setLoading(true);
+    const response = await fetch(`https://fakestoreapi.com/products`);
+    const data = await response.json();
+
+    if (data) {
+      setLoading(false);
+      setProducts(data);
+    }
+  }
+
+  useEffect(() => {
+    fetchListOfProducts();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <div className="min-h-screen w-full flex justify-center item-center">
+          <Circles
+            height={"120"}
+            width={"120"}
+            color="rgb(127,29,29)"
+            visible={true}
+          />
+        </div>
+      ) : null}
+    </div>
+  );
 }
