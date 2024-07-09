fa2f8c1c9566e848e227176943ed8dfc90adca6a
Austen Turner <austenpturner@msn.com>
2024-07-09 16:36:33 -0700

    add Provider to main and add cart actions

--- src/store/slices/cart-slice.js
+++ src/store/slices/cart-slice.js
@@ -10,7 +10,13 @@ const initialState = [];
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart(state, action) {
+      state.push(action.payload);
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
